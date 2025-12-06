b19812b6c5a1b2698156d29aca6c2702a0ba0cdc
Louis Jannett <louis.jannett@rub.de>
2025-12-06 20:33:32 +0100

    fix: remove secure cookies for proxies and local setups

--- src/app.ts
+++ src/app.ts
@@ -33,7 +33,6 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: process.env.NODE_ENV === 'production',
       httpOnly: true,
       maxAge: 24 * 60 * 60 * 1000, // 24 hours
     },
